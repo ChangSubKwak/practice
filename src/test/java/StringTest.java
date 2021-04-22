b8a920fc1e8c5f0526d188eb93be61c886e6b1fa
ChangSubKwak <paradise1732@gmail.com>
2021-04-22 20:23:10 +0900

    docs: add
    add files

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -7,10 +8,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
 	@Test
-	public void splitTest() {
+	public void splitTest1_1() {
 		String input = "1,2";							// given
-		String[] t1 = input.split(",");				// when
-		assertThat(t1).contains("1");					// then  #1
-		assertThat(t1).containsExactly("1","2");		// then  #2
+		String[] s = input.split(",");				// when
+		assertThat(s).containsExactly("1","2");		// then
+	}
+
+	@Test
+	public void splitTest1_2() {
+		String input = "1";							// given
+		String[] s = input.split(",");			// when
+		assertThat(s).contains("1");				// then
+	}
+
+	@Test
+	public void splitTest2() {
+		String input = "(1,2)";										// given
+		String s = input.substring(1, input.length() - 1);	// when
+		assertThat(s).isEqualTo("1,2");								// then
+	}
+
+	@Test
+	@DisplayName("String의 chrAt()메소드를 활용해 특정 위치의 문자를 가져오는 테스트")
+	public void splitTest3_1() {
+		String input = "abc";										// given
+		//String s = input.substring(1, input.length() - 1);	// when
+		//assertThat(s).isEqualTo("1,2");								// then
+		assertThat
 	}
 }
