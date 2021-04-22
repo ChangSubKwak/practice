63ee82ec33c5cbb82e15a55c390e6ce463234f95
ChangSubKwak <paradise1732@gmail.com>
2021-04-22 20:41:00 +0900

    test: add
    add test feat

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 //import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringTest {
@@ -31,9 +32,25 @@ public class StringTest {
 	@Test
 	@DisplayName("String의 chrAt()메소드를 활용해 특정 위치의 문자를 가져오는 테스트")
 	public void splitTest3_1() {
-		String input = "abc";										// given
-		//String s = input.substring(1, input.length() - 1);	// when
-		//assertThat(s).isEqualTo("1,2");								// then
-		assertThat
+		String input = "abc";					// given
+		char c = input.charAt(0);				// when #1
+		assertThat(c).isEqualTo('a');			// then #1
+
+		c = input.charAt(1);					// when #2
+		assertThat(c).isEqualTo('b');			// then #2
+
+		c = input.charAt(2);					// when #2
+		assertThat(c).isEqualTo('c');			// then #2
+	}
+
+	@Test
+	@DisplayName("String의 charAt() 사용시 StringIndexOutOfBoundsException 발생 테스트")
+	public void splitTest3_2() {
+		String input = "abc";					// given
+		int idx = 3;							// when
+		assertThatThrownBy(() -> {				// then
+			input.charAt(idx);
+		}).isInstanceOf(IndexOutOfBoundsException.class)
+		.hasMessageContaining("String index out of range: " + idx);
 	}
 }
